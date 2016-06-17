3bf6e38dcc9ed05cd2878f5b66846fbd8b548e94
Echarger1 <echarger1@gmail.com>
2016-06-17 15:19:13 -0400

    Added Main Menu

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -8,8 +8,41 @@ namespace SummerSchool
 {
     class Program
     {
+        static string[] Students = new string[15];
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1.Enroll a student");
+                Console.WriteLine("2.Unenroll a student");
+                Console.WriteLine("3.Print student list");
+                Console.WriteLine("4.Exit");
+
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Enrollment();
+                }
+
+                
+            }
+
         }
+        static void Exit()
+        {
+            Console.ReadKey();
+
+        }
+
+        static void Enrollment()
+        {
+
+        }
+        
+
+
     }
 }
